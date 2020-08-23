5edbeca115097ea8a5678d19485f522c723b960e
KodaiD <bb_dkoudai@yahoo.co.jp>
2020-08-23 20:39:20 +0900

    in-memory-db

--- main.go
+++ main.go
@@ -2,32 +2,56 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/binary"
 	"fmt"
+	"log"
 	"os"
 )
 
-const (
-	META_COMMAND_SUCCESS = 0
-	META_COMMAND_UNRECOGNIZED_COMMAND = 1
-	META_COMMAND_EXIT = 2
-)
+/*
+limitations
+- support two operations: inserting a row and printing all rows
+- reside only in memory (no persistence to disk)
+- support a single, hard-coded table
 
-const (
-	PREPARE_SUCCESS = 0
-	PREPARE_UNRECOGNIZED_STATEMENT = 1
-)
+data structure
+- Store rows in blocks of memory called pages
+- Each page stores as many rows as it can fit
+- Rows are serialized into a compact representation with each page
+- Pages are only allocated as needed
+- Keep a fixed-size array of pointers to pages
+*/
 
-const (
-	STATEMENT_INSERT = 0
-	STATEMENT_SELECT = 1
-)
+type MetaCommandResult int
+type PrepareResult int
+type StatementType int
+type ExecuteResult int
 
 type Statement struct {
-	Type int
+	Type 		StatementType
+	RowToInsert Row
+}
+
+type Row struct {
+	ID			uint32
+	UserName	[COLUMN_USERNAME_SIZE]byte
+	Email		[COLUMN_EMAIL_SIZE]byte
+}
+
+type Page struct {
+	rows [ROWS_PER_PAGE]*Row
+}
+
+type Table struct {
+	NumRows uint32
+	Pages [TABLE_MAX_PAGES]*Page
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	table := newTable()
+	var buf bytes.Buffer
 	for {
 		printPrompt()
 		if scanner.Scan() {
@@ -47,12 +71,21 @@ func main() {
 			switch prepareStatement(inputBuffer, statement) {
 			case PREPARE_SUCCESS:
 				break
+			case PREPARE_SYNTAXERROR:
+				fmt.Println("Syntax error. Could not parse statement.")
+				continue
 			case PREPARE_UNRECOGNIZED_STATEMENT:
 				fmt.Printf("Unrecognized keyword at start of '%s'.\n", inputBuffer)
 				continue
 			}
-			executeStatement(statement)
-			fmt.Println("Executed.")
+			switch executeStatement(statement, table, &buf) {
+			case EXECUTE_SUCCESS:
+				fmt.Println("Executed.")
+				break
+			case EXECUTE_TABLE_FULL:
+				fmt.Println("Error: Table full.")
+				break
+			}
 		}
 	}
 }
@@ -61,7 +94,7 @@ func printPrompt() {
 	fmt.Print("db > ")
 }
 
-func doMetaCommand(inputBuffer string) int {
+func doMetaCommand(inputBuffer string) MetaCommandResult {
 	if inputBuffer == ".exit" {
 		return META_COMMAND_EXIT
 	} else {
@@ -69,9 +102,15 @@ func doMetaCommand(inputBuffer string) int {
 	}
 }
 
-func prepareStatement(inputBuffer string, statement *Statement) int {
+func prepareStatement(inputBuffer string, statement *Statement) PrepareResult {
 	if inputBuffer[:6] == "insert" {
 		statement.Type = STATEMENT_INSERT
+		argsAssigned, err := fmt.Sscanf(inputBuffer, "insert %d %s %s", &statement.RowToInsert.ID,
+									&statement.RowToInsert.UserName, &statement.RowToInsert.Email)
+		if argsAssigned < 3 {
+			log.Println(err)
+			return PREPARE_SYNTAXERROR
+		}
 		return PREPARE_SUCCESS
 	}
 	if inputBuffer == "select" {
@@ -81,13 +120,67 @@ func prepareStatement(inputBuffer string, statement *Statement) int {
 	return PREPARE_UNRECOGNIZED_STATEMENT
 }
 
-func executeStatement(statement *Statement) {
+func executeInsert(statement *Statement, table *Table, buf *bytes.Buffer) ExecuteResult {
+	if table.NumRows >= TABLE_MAX_ROWS {
+		return EXECUTE_TABLE_FULL
+	}
+	row := &statement.RowToInsert
+	err := row.serializeRow(buf)
+	if err != nil {
+		log.Println(err)
+	}
+	table.NumRows += 1
+	return EXECUTE_SUCCESS
+}
+
+func executeSelect(statement *Statement, table *Table, buf *bytes.Buffer) ExecuteResult {
+	row := &statement.RowToInsert
+	for i := uint32(0); i < table.NumRows; i++ {
+		err := row.deserializeRow(buf)
+		if err != nil {
+			log.Println(err)
+		}
+		row.printRow()
+	}
+	return EXECUTE_SUCCESS
+}
+
+func executeStatement(statement *Statement, table *Table, buf *bytes.Buffer) ExecuteResult {
 	switch statement.Type {
 	case STATEMENT_INSERT:
-		fmt.Println("This is where we would do an insert.")
-		break
+		return executeInsert(statement, table, buf)
 	case STATEMENT_SELECT:
 		fmt.Println("This is where we would do a select.")
-		break
+		return executeSelect(statement, table, buf)
 	}
+}
+
+func (r *Row) serializeRow(buf *bytes.Buffer) error {
+	err := binary.Write(buf, binary.BigEndian, r)
+	return err
+}
+
+func (r *Row) deserializeRow(buf *bytes.Buffer) error {
+	err := binary.Read(buf, binary.BigEndian, r)
+	return err
+}
+
+func newTable() *Table {
+	table := &Table{
+		NumRows: 0,
+	}
+	return table
+}
+
+func (r *Row) printRow() {
+	fmt.Printf("(%d, %s, %s)\n", r.ID, r.UserName, r.Email)
+}
+
+func (table *Table) rowSlot(rowNum uint32) uint32 {
+	pageNum := rowNum / ROWS_PER_PAGE
+	page := table.Pages[pageNum]
+	if page == nil {
+		page = table.Pages[pageNum]
+	}
+	return pageNum
 }
